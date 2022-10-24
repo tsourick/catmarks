2befd26bcfcdd246b81d43472e570fffa1a47db3
Roman Tsourick <tsourick@gmail.com>
2022-10-24 08:48:29 +0300

    feat(widget): add root&none buttons

--- content/cmp.Widget.js
+++ content/cmp.Widget.js
@@ -16,17 +16,31 @@ class Widget extends React.Component {
 	
 	render() {
 		let children = [];
+		const createClickHandler = (bmKey) => (e) => { this.onClick(bmKey) };
 		
 		this.props.list?.forEach((item) => {
 			let props = {
 				className: this.props.selectedKey == item.bmKey ? 'selected' : null, 
 				style: {"--color": item.color}, 
-				onClick: ((bmKey) => (e) => { this.onClick(bmKey) })(item.bmKey)
+				onClick: createClickHandler(item.bmKey)
 			};
 			
 			children.push(['li', props, `${item.name}`]);
 		});
 		
+		children.push(
+			['hr', {style: {width: '100%', margin: '0', padding: '0'}}, null],
+			['li', {
+				className: false ? 'selected' : null, 
+				style: {"--color": 'blue'}, 
+				onClick: createClickHandler('somerootkeyhere')
+			}, `root`],
+			['li', {
+				className: false ? 'selected' : null, 
+				style: {"--color": 'gray'}, 
+				onClick: createClickHandler(null)
+			}, `none`],
+		);
 		
 		return e('div', {}, list([
 			['ul', {}, list(children)]
